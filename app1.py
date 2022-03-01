e390b5e335dc1fcee6903b3fe779aa63d4755db8
serhat-project <sucar661@gmail.com>
2022-03-01 19:29:46 +0300

    last files added

--- app1.py
+++ app1.py
@@ -1,10 +1,14 @@
-from flask import Flask  
-app = Flask(__name__)  
-      
-def about():  
-    return "This is about page";  
-      
-app.add_url_rule("/about","about",about)  
-      
-if __name__ =="__main__":  
-    app.run(debug = True)  
+import random
+from random import choice
+
+name = (input('Please enter your full name : ').lower()).replace(' ','')
+numbers = range(10)
+num=str(random.choice(numbers))
+chars = name + num
+length = 8
+
+print(''.join([choice(chars) for i in range(length)]))
+
+
+
+
